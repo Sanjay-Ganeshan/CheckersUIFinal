377a6c66f6632f85e7fe7c807b61206284420324
Sanjay Ganeshan <sangan@live.com>
2016-06-29 20:29:25 -0400

    Published and signed

--- CheckerUI/Properties/AssemblyInfo.cs
+++ CheckerUI/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CheckerUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Web Checkers - UI")]
+[assembly: AssemblyDescription("A UI for the game of Checkers, coded by Sanjay, rules by Natasha")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Hypernova Software")]
 [assembly: AssemblyProduct("CheckerUI")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("")]
+
